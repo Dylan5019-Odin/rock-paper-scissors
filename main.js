79d15bf1821c57ccb6a64bd3d9ce2be7c9c8fd26
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2022-12-28 19:13:27 -0600

    Set up user Input, set up game logic, set up test game

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-
+//Choices that the computer can select from
 const choices = ['rock', 'paper', 'scissors'];
 
+  const beatenBy = {
+    "scissors": "paper",
+    "paper": "rock",
+    "rock" : "scissors",
+  };
+
+// Function that generates a random choice from the choices array
 const getComputerChoice = () => {
-   return (choice = choices[Math.floor(Math.random() * choices.length)]);
+   return choices[Math.floor(Math.random() * choices.length)];
 };
 
-console.log(getComputerChoice());
+
+    
+//Function to promnt the user for input
+const playRound = (playerChoice, computerChoice) => { 
+    playerChoice = playerChoice.toLowerCase();
+
+    if (playerChoice === computerChoice) {
+        return "Draw!"
+    } else if (beatenBy[playerChoice] === computerChoice) {
+        return `You Win! ${playerChoice} beats ${computerChoice}!`
+    } else {
+        return `You lose! ${playerChoice} losses to ${computerChoice}!`
+    }
+  
+}
 
 
 
+//Get user input, and generate a random choice for the computer
+    let computerChoice = getComputerChoice();
+    let playerChoice = prompt("Enter your choice");
+
+
+// play a round
+console.log(playRound(playerChoice, computerChoice));
